9f8bba35c401d722f7ffabf327d784f1f2d2e9ac
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-06-11 16:56:07 +0500

    add Login components

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,55 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    // setEmail("");
+    // setPassword("");
+    // setDisplayName("");
+  };
+
+  return (
+    <form onSubmit={handleForm} className={styles["login-form"]}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+      </label>
+
+      {/* {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {!isPending && <button className="btn">Signup</button>}
+      {error && <div>{error}</div>} */}
+    </form>
+  );
 }
 
 export default Login;
