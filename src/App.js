a3ce944f31598f7ea10c4dc9fff707040fdecee3
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-06-13 18:39:43 +0500

    the time you visit the site suddenly goes to the main page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Navbar from "./components/Navbar";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
+  const { authStatus, user } = useAuth();
+  console.log(user);
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      {authStatus && (
+        <>
+          <Router>
+            <Navbar />
+            <Routes>
+              {user && <Route path="/" element={<Home />} />}
+              {!user && <Route element={<Navigate to="/login" />} />}
+              {!user && <Route path="/login" element={<Login />} />}
+              {user && <Route path="/login" element={<Navigate to="/" />} />}
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </Router>
+        </>
+      )}
     </div>
   );
 }
